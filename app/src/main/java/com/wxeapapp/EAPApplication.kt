56f205f436174c6470a0b1e1259437d5558e3fc2
nickming <962570483@qq.com>
2017-09-03 12:14:18 +0800

    集成了Sophix热修复框架

--- app/src/main/java/com/wxeapapp/EAPApplication.kt
+++ app/src/main/java/com/wxeapapp/EAPApplication.kt
@@ -1,9 +1,12 @@
 package com.wxeapapp
 
 import android.app.Application
+import com.taobao.sophix.PatchStatus
+import com.taobao.sophix.SophixManager
 import com.wxeapapp.api.ApiUtils
 import com.wxeapapp.utils.L
 import com.wxeapapp.utils.push.XgPushUtils
+
 
 /**
  * class description here
@@ -30,6 +33,25 @@ class EAPApplication : Application() {
             token ->
             L("register success:" + token)
         })
+        val appVersion = packageManager.getPackageInfo(packageName, 0).versionName
+        // initialize最好放在attachBaseContext最前面
+        SophixManager.getInstance().setContext(this)
+                .setAppVersion(appVersion)
+                .setAesKey(null)
+                .setEnableDebug(true)
+                .setPatchLoadStatusStub { mode, code, info, handlePatchVersion ->
+                    // 补丁加载回调通知
+                    if (code == PatchStatus.CODE_LOAD_SUCCESS) {
+                        // 表明补丁加载成功
+                    } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
+                        // 表明新补丁生效需要重启. 开发者可提示用户或者强制重启;
+                        // 建议: 用户可以监听进入后台事件, 然后调用killProcessSafely自杀，以此加快应用补丁，详见1.3.2.3
+                    } else {
+                        // 其它错误信息, 查看PatchStatus类说明
+                    }
+                }.initialize()
+        // queryAndLoadNewPatch不可放在attachBaseContext 中，否则无网络权限，建议放在后面任意时刻，如onCreate中
+        SophixManager.getInstance().queryAndLoadNewPatch()
 
     }
 }
